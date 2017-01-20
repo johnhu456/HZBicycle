bf4ea99424e14b6c1e2949afc9476a2043186231
Hu Xiang <johnhu456@163.com>
2017-01-20 15:57:04 +0800

    1、修改自行车图标 2、增加实时导航界面

---
+++ HZBicycle/ViewController/HBRealTimeNaviController.h
@@ -0,0 +1,16 @@
+//
+//  HBRealTimeNaviController.h
+//  HZBicycle
+//
+//  Created by 胡翔 on 2017/1/20.
+//  Copyright © 2017年 MADAO. All rights reserved.
+//
+
+#import "HBBaseViewController.h"
+#import "HBNaviManager.h"
+
+@interface HBRealTimeNaviController : HBBaseViewController
+
+- (instancetype)initWithNaviType:(HBNaviType)naviType;
+
+@end
